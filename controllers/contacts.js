98d2a06988293a2110ff9ecb98847a7507ab2a4e
OksanaTur <ksyturkevych@gmail.com>
2023-05-22 11:54:16 +0400

    func

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,45 +1,55 @@
-const contacts = require("../models/contacts");
+const {Contact} = require("../models/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
-    const result = await contacts.listContacts();
+  const result = await Contact.find({}, "-createdAt -updatedAt");
     res.json(result);
 };
 
 const getContactById = async (req, res) => {
     const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
-        throw HttpError(404, "Not Found")
+        throw HttpError(404, `Book with ${contactId} not found`)
     }
     res.json(result);
 };
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
 
   if (!result) {
-    throw HttpError(404, "Not Found");
+    throw HttpError(404, `Book with ${contactId} not found`);
   }
 
   res.status(200).json({
-    message: "Delete completed",
+    message: "Delete success",
   });
 };
 
 const addContact = async (req, res) => {
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
 
   if (!result) {
-    throw HttpError(404, "Not Found");
+    throw HttpError(404, `Book with ${contactId} not found`);
+  }
+  res.json(result);
+};
+
+const updateFavorite = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+
+  if (!result) {
+    throw HttpError(404, `Book with ${contactId} not found`);
   }
   res.json(result);
 };
@@ -50,4 +60,5 @@ module.exports = {
   deleteContactByID: ctrlWrapper(removeContact),
   addContact: ctrlWrapper(addContact),
   updateContact: ctrlWrapper(updateContact),
+  updateFavorite: ctrlWrapper(updateFavorite),
 };
